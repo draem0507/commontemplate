d1e31c3aac413e56b209ed6e90baa44d4efffbef
liangfei0201 <liangfei0201@8dadb90d-c23f-0410-a79a-e763b8a9fd85>
2008-03-09 05:32:10 +0000

    将inline改成embed

--- commontemplate/src/main/java/org/commontemplate/standard/directive/include/EmbedTemplateDirectiveHandler.java
+++ commontemplate/src/main/java/org/commontemplate/standard/directive/include/EmbedTemplateDirectiveHandler.java
@@ -12,12 +12,12 @@ import org.commontemplate.util.Assert;
 /**
  * 内嵌另一模板，被内嵌的模板共享当前Context，可直接取变量
  * 语法：
- * $inline{"xxx.ctl"}
+ * $embed{"xxx.ctl"}
  *
  * @author liangfei0201@163.com
  *
  */
-public class InlineTemplateDirectiveHandler implements LineDirectiveHandler {
+public class EmbedTemplateDirectiveHandler implements LineDirectiveHandler {
 
 	private static final long serialVersionUID = 1L;
 
@@ -28,16 +28,16 @@ public class InlineTemplateDirectiveHandler implements LineDirectiveHandler {
 			templateName = (String)param;
 		} else if (param instanceof List) {
 			List list = (List)param;
-			Assert.assertTrue(list.size() == 2, "inline参数列表错误!");
-			Assert.assertTrue(list.get(0) instanceof String, "inline参数列表错误!");
-			Assert.assertTrue(list.get(1) instanceof String, "inline参数列表错误!");
+			Assert.assertTrue(list.size() == 2, "embed参数列表错误!");
+			Assert.assertTrue(list.get(0) instanceof String, "embed参数列表错误!");
+			Assert.assertTrue(list.get(1) instanceof String, "embed参数列表错误!");
 			templateName = (String)list.get(0);
 			templateEncoding = (String)list.get(1);
 		} else {
-			Assert.fail("inline参数列表错误!");
+			Assert.fail("embed参数列表错误!");
 		}
 		if (templateName == null || templateName.trim().length() == 0)
-			throw new RuntimeException("inline模板名称不能为空!");
+			throw new RuntimeException("embed模板名称不能为空!");
 		String zoneName = null;
 		int index = templateName.indexOf('#');
 		if (index >= 0) {
@@ -45,7 +45,7 @@ public class InlineTemplateDirectiveHandler implements LineDirectiveHandler {
 			templateName = templateName.substring(0, index);
 		}
 		if (context.containsTemplate(context.relateTemplateName(templateName))) // 循环内嵌检测
-			throw new RuntimeException("内嵌(inline)模板时出现循环内嵌！请检查从模板 " + templateName + " 到模板 " + context.getCurrentTemplate() + " 之间的内嵌关系。");
+			throw new RuntimeException("内嵌(embed)模板时出现循环内嵌！请检查从模板 " + templateName + " 到模板 " + context.getCurrentTemplate() + " 之间的内嵌关系。");
 		Template template = context.getTemplate(templateName, templateEncoding); // getTemplate的后验条件已保证不返回null
 		if (zoneName != null && zoneName.length() > 0) {
 			List elements = ElementsVisitor.findElements(template, "zone", zoneName);
--- commontemplate/src/test/resources/integration/template/inline.ctl
+++
@@ -1,2 +0,0 @@
-$var{name : "liangfei"}
-$inline{"../included/welcome.mm"}
