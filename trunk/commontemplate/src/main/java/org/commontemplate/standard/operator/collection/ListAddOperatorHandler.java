a34017fa1e52d0b9b67959d2219428e5f031b1fd
liangfei0201 <liangfei0201@8dadb90d-c23f-0410-a79a-e763b8a9fd85>
2008-03-13 12:39:59 +0000

    修复ListAdd与MapAdd的异常处理

--- trunk/commontemplate/src/main/java/org/commontemplate/standard/operator/collection/ListAddOperatorHandler.java
+++ trunk/commontemplate/src/main/java/org/commontemplate/standard/operator/collection/ListAddOperatorHandler.java
@@ -1,13 +1,13 @@
 package org.commontemplate.standard.operator.collection;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 import org.commontemplate.standard.operator.BinaryOperatorHandlerSupport;
-import org.commontemplate.util.Assert;
 
 /**
- * 集合数据相加
- * 
+ * 两个集合数据相加
+ *
  * @author liangfei0201@163.com
  *
  */
@@ -18,19 +18,26 @@ public class ListAddOperatorHandler extends BinaryOperatorHandlerSupport {
 	public ListAddOperatorHandler() {
 		super(Collection.class, Collection.class);
 	}
-	
+
 	public Object doEvaluate(Object leftOperand, Object rightOperand) throws Exception {
 		Collection leftCollection = (Collection)leftOperand;
 		Collection rightCollection = (Collection)rightOperand;
-		
-		if(!leftCollection.getClass().getName().equals(rightCollection.getClass().getName())) {
-			Assert.fail("leftOperand and rightOperand is not same Implement Class!");
+
+		/* 允许不同类型集合相加
+		if(! leftCollection.getClass().getName().equals(rightCollection.getClass().getName())) {
+			Assert.fail("leftOperand and rightOperand is not same implement class!");
+		}*/
+
+		Collection sumCollection = null; // 新建Collection对象, 以保证不改变原有数据模型
+		try {
+			sumCollection = (Collection) leftCollection.getClass().newInstance();
+		} catch (Exception e) { // 当左集合实现类没有无参构造子时, 试用右集合实现类
+			try {
+				sumCollection = (Collection) rightCollection.getClass().newInstance();
+			} catch (Exception e2) { // 默认采用ArrayList
+				sumCollection = new ArrayList(leftCollection.size() + rightCollection.size());
+			}
 		}
-		
-		Class clazz = leftCollection.getClass();
-		
-		//Collection sumCollection = new ArrayList(leftCollection.size() + rightCollection.size()); // 新建Collection对象, 以保证不改变原有数据模型
-		Collection sumCollection = (Collection) clazz.newInstance();
 		sumCollection.addAll(leftCollection);
 		sumCollection.addAll(rightCollection);
 		return sumCollection;
