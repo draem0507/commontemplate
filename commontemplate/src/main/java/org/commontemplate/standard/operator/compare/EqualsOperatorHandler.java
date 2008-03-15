bea51588ae4bbf7697156b3e23c632b30766f44b
liangfei0201 <liangfei0201@8dadb90d-c23f-0410-a79a-e763b8a9fd85>
2008-03-15 14:04:22 +0000

    添加注释

--- commontemplate/src/main/java/org/commontemplate/standard/operator/compare/EqualsOperatorHandler.java
+++ commontemplate/src/main/java/org/commontemplate/standard/operator/compare/EqualsOperatorHandler.java
@@ -3,8 +3,10 @@ package org.commontemplate.standard.operator.compare;
 import org.commontemplate.standard.operator.BinaryOperatorHandlerSupport;
 
 /**
- * "等于"操作符处理器
- * 
+ * 值相等比较操作符: "=="<br/>
+ * 如: $if{obj1 == obj2} <br/>
+ * 注: 这里需处理char与string的比较. 如: $if{ch == "A"} (假设ch是char类型变量)<br/>
+ *
  * @see java.lang.Object#equals(Object)
  * @author liangfei0201@163.com
  *
