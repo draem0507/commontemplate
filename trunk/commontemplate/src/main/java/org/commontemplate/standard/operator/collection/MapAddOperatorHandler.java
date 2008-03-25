99a3747a96e27c195c0d2e3b746659525e76bb35
liangfei0201 <liangfei0201@8dadb90d-c23f-0410-a79a-e763b8a9fd85>
2008-03-25 13:20:34 +0000

    重构集合相加

--- trunk/commontemplate/src/main/java/org/commontemplate/standard/operator/collection/MapAddOperatorHandler.java
+++ trunk/commontemplate/src/main/java/org/commontemplate/standard/operator/collection/MapAddOperatorHandler.java
@@ -24,23 +24,22 @@ public class MapAddOperatorHandler extends BinaryOperatorHandlerSupport {
 		Map leftMap = (Map)leftOperand;
 		Map rightMap = (Map)rightOperand;
 
-		/* 允许不同Map实现类相加
-		if(! leftMap.getClass().getName().equals(rightMap.getClass().getName())) {
-			Assert.fail("leftOperand and rightOperand is not same implement class!");
-		}*/
-
 		Map sumMap = null;// 新建Map对象, 以保证不改变原有数据模型
 		try {
 			sumMap = (Map) leftMap.getClass().newInstance();
+			sumMap.putAll(leftMap); // 可能有Map实现不支持putAll操作, 所以放在try内
+			sumMap.putAll(rightMap);
 		} catch (Exception e) { // 当左Map实现类没有无参构造子时, 试用右Map实现类
 			try {
 				sumMap = (Map) rightMap.getClass().newInstance();
+				sumMap.putAll(leftMap);
+				sumMap.putAll(rightMap);
 			} catch (Exception e2) { // 默认采用HashMap
 				sumMap = new HashMap(leftMap.size() + rightMap.size());
+				sumMap.putAll(leftMap);
+				sumMap.putAll(rightMap);
 			}
 		}
-		sumMap.putAll(leftMap);
-		sumMap.putAll(rightMap);
 		return sumMap;
 	}
 
