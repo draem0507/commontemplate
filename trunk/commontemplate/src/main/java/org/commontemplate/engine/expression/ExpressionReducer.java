6276b7a1df57aae55279142986da1154af444a18
liangfei0201 <liangfei0201@8dadb90d-c23f-0410-a79a-e763b8a9fd85>
2008-07-17 06:22:38 +0000

    加入对表达式未结束括号的检查.

--- trunk/commontemplate/src/main/java/org/commontemplate/engine/expression/ExpressionReducer.java
+++ trunk/commontemplate/src/main/java/org/commontemplate/engine/expression/ExpressionReducer.java
@@ -42,19 +42,21 @@ final class ExpressionReducer {
 			Expression expression = (Expression) expressions.get(i);
 
 			// 弹栈
-			if (expression == Parenthesis.RIGHT_PARENTHESIS)
+			if (expression == Parenthesis.RIGHT_PARENTHESIS) {
 				while (expressionStack.popOperator() != Parenthesis.LEFT_PARENTHESIS); // 一直弹出到最近的一个左括号为止
-			else if (expression instanceof BinaryOperator)
-				while (isHighPriority((BinaryOperator) expression,
-						expressionStack.peekOperator()))
+			} else if (expression instanceof BinaryOperator) {
+				while (isHighPriority((BinaryOperator) expression, expressionStack.peekOperator())) {
 					expressionStack.popOperator(); // 将优先级高于及等于当前操作符的弹出
+				}
+			}
 
 			// 压栈
-			if (expression != Parenthesis.RIGHT_PARENTHESIS) // 右括号不入栈
+			if (expression != Parenthesis.RIGHT_PARENTHESIS) { // 右括号不入栈
 				if (expression instanceof Operator)
 					expressionStack.pushOperator((Operator) expression); // 压入操作符
 				else
 					expressionStack.pushParameter(expression); // 压入参数
+			}
 		}
 		return expressionStack.popResult();
 	}
@@ -78,6 +80,139 @@ final class ExpressionReducer {
 			return true;
 		return false;
 	}
+
+	// 表达式归约辅助栈
+	private static final class ReduceStack {
+
+		// 操作符栈
+		private Stack operatorStack = new LinkedStack();
+
+		// 参数栈
+		private Stack parameterStack = new LinkedStack();
+
+		/**
+		 * 压入操作符
+		 *
+		 * @param operator
+		 *            操作符
+		 */
+		void pushOperator(Operator operator) {
+			operatorStack.push(operator);
+		}
+
+		/**
+		 * 压入参数
+		 *
+		 * @param parameter
+		 *            参数
+		 */
+		void pushParameter(Expression parameter) {
+			parameterStack.push(parameter);
+		}
+
+		/**
+		 * 窥取栈顶操作符
+		 *
+		 * @return 操作符
+		 */
+		Operator peekOperator() {
+			if (operatorStack.isEmpty())
+				return null;
+			return (Operator) operatorStack.peek();
+		}
+
+		/**
+		 * 弹出(组装好的)操作符
+		 *
+		 * @return 操作符
+		 */
+		Operator popOperator() {
+			if (operatorStack.isEmpty())
+				throw I18nExceptionFactory.createIllegalStateException("ExpressionReducer.miss.left.parenthesis");
+
+			Operator operator = (Operator) operatorStack.pop();
+			// 从参数栈弹出所需参数组装操作表达式
+			if (operator instanceof BinaryOperator) {
+				BinaryOperator binaryOperator = (BinaryOperator) operator;
+				Expression rightParameter = (Expression) parameterStack.pop(); // 调整参数顺序
+				Expression leftParameter = (Expression) parameterStack.pop();
+				parameterStack.push(populateBinaryOperator(
+						(BinaryOperatorImpl) binaryOperator, leftParameter,
+						rightParameter)); // 将组装好的表达式作为参数压入
+			} else if (operator instanceof UnaryOperator) {
+				UnaryOperator unaryOperator = (UnaryOperator) operator;
+				Expression parameter = (Expression) parameterStack.pop();
+				parameterStack.push(populateUnaryOperator(
+						(UnaryOperatorImpl) unaryOperator, parameter)); // 将组装好的表达式作为参数压入
+			}
+			return operator;
+		}
+
+		// 组装二元操作符
+		private Expression populateBinaryOperator(
+				BinaryOperatorImpl binaryOperator, Expression leftParameter,
+				Expression rightParameter) {
+			if (leftParameter instanceof Variable
+					&& binaryOperator.isLeftOperandNamed())
+				leftParameter = new ConstantImpl(((Variable) leftParameter)
+						.getName(), leftParameter.getLocation());
+			if (rightParameter instanceof Variable
+					&& binaryOperator.isRightOperandNamed())
+				rightParameter = new ConstantImpl(((Variable) rightParameter)
+						.getName(), rightParameter.getLocation());
+			binaryOperator.setOperands(leftParameter, rightParameter);
+			try {
+				// 常量优化算法，将常量先计算，如：
+				// 2 * 3 + coins 将被优化为 6 + coins
+				// (注: 表达式应尽量把常量计算写在前面, 如: 24 * 60 * days, 而不要用 days * 24 * 60)
+				if (leftParameter instanceof Constant
+						&& rightParameter instanceof Constant)
+					return new ConstantImpl(binaryOperator.evaluate(null),
+							binaryOperator.getLocation());
+			} catch (Exception e) {
+				// 抛出任何异常都表示其不支持优化
+			}
+			return binaryOperator;
+		}
+
+		// 组装一元操作符
+		private Expression populateUnaryOperator(
+				UnaryOperatorImpl unaryOperator, Expression parameter) {
+			if (parameter instanceof Variable && unaryOperator.isOperandNamed())
+				parameter = new ConstantImpl(((Variable) parameter).getName(),
+						parameter.getLocation());
+			unaryOperator.setOperand(parameter);
+			try {
+				// 常量优化算法，将常量先计算，如：
+				// ! true 将被优化为 false
+				if (parameter instanceof Constant)
+					return new ConstantImpl(unaryOperator.evaluate(null),
+							unaryOperator.getLocation());
+			} catch (Exception e) {
+				// 抛出任何异常都表示其不支持优化
+			}
+			return unaryOperator;
+		}
+
+		/**
+		 * 弹出(最后的)结果
+		 *
+		 * @return 结果
+		 */
+		Expression popResult() {
+			while (!operatorStack.isEmpty()) {
+				Operator operator = popOperator(); // 弹出栈中所有操作符
+				if (operator == Parenthesis.LEFT_PARENTHESIS)
+					throw I18nExceptionFactory.createIllegalStateException("ExpressionReducer.miss.right.parenthesis");
+			}
+			Expression result = (Expression) parameterStack.pop();
+			Assert.assertTrue(parameterStack.isEmpty(), "ExpressionReducer.operator.miss.parameter"); // 后验条件
+			return result;
+		}
+
+	}
+
+	// --------- 以下为YanRong对四则运算做的预优化处理 --------
 
 	/**
 	 * 预优化表达式，原则是常量的计算进行优化。<br>
@@ -300,134 +435,6 @@ final class ExpressionReducer {
 		}
 
 		return false;
-	}
-
-	// 表达式归约辅助栈
-	private static final class ReduceStack {
-
-		// 操作符栈
-		private Stack operatorStack = new LinkedStack();
-
-		// 参数栈
-		private Stack parameterStack = new LinkedStack();
-
-		/**
-		 * 压入操作符
-		 *
-		 * @param operator
-		 *            操作符
-		 */
-		void pushOperator(Operator operator) {
-			operatorStack.push(operator);
-		}
-
-		/**
-		 * 压入参数
-		 *
-		 * @param parameter
-		 *            参数
-		 */
-		void pushParameter(Expression parameter) {
-			parameterStack.push(parameter);
-		}
-
-		/**
-		 * 窥取栈顶操作符
-		 *
-		 * @return 操作符
-		 */
-		Operator peekOperator() {
-			if (operatorStack.isEmpty())
-				return null;
-			return (Operator) operatorStack.peek();
-		}
-
-		/**
-		 * 弹出(组装好的)操作符
-		 *
-		 * @return 操作符
-		 */
-		Operator popOperator() {
-			if (operatorStack.isEmpty())
-				throw I18nExceptionFactory.createIllegalStateException("ExpressionReducer.miss.parenthesis");
-
-			Operator operator = (Operator) operatorStack.pop();
-			// 从参数栈弹出所需参数组装操作表达式
-			if (operator instanceof BinaryOperator) {
-				BinaryOperator binaryOperator = (BinaryOperator) operator;
-				Expression rightParameter = (Expression) parameterStack.pop(); // 调整参数顺序
-				Expression leftParameter = (Expression) parameterStack.pop();
-				parameterStack.push(populateBinaryOperator(
-						(BinaryOperatorImpl) binaryOperator, leftParameter,
-						rightParameter)); // 将组装好的表达式作为参数压入
-			} else if (operator instanceof UnaryOperator) {
-				UnaryOperator unaryOperator = (UnaryOperator) operator;
-				Expression parameter = (Expression) parameterStack.pop();
-				parameterStack.push(populateUnaryOperator(
-						(UnaryOperatorImpl) unaryOperator, parameter)); // 将组装好的表达式作为参数压入
-			}
-			return operator;
-		}
-
-		// 组装二元操作符
-		private Expression populateBinaryOperator(
-				BinaryOperatorImpl binaryOperator, Expression leftParameter,
-				Expression rightParameter) {
-			if (leftParameter instanceof Variable
-					&& binaryOperator.isLeftOperandNamed())
-				leftParameter = new ConstantImpl(((Variable) leftParameter)
-						.getName(), leftParameter.getLocation());
-			if (rightParameter instanceof Variable
-					&& binaryOperator.isRightOperandNamed())
-				rightParameter = new ConstantImpl(((Variable) rightParameter)
-						.getName(), rightParameter.getLocation());
-			binaryOperator.setOperands(leftParameter, rightParameter);
-			try {
-				// 常量优化算法，将常量先计算，如：
-				// 2 * 3 + coins 将被优化为 6 + coins
-				// (注: 表达式应尽量把常量计算写在前面, 如: 24 * 60 * days, 而不要用 days * 24 * 60)
-				if (leftParameter instanceof Constant
-						&& rightParameter instanceof Constant)
-					return new ConstantImpl(binaryOperator.evaluate(null),
-							binaryOperator.getLocation());
-			} catch (Exception e) {
-				// 抛出任何异常都表示其不支持优化
-			}
-			return binaryOperator;
-		}
-
-		// 组装一元操作符
-		private Expression populateUnaryOperator(
-				UnaryOperatorImpl unaryOperator, Expression parameter) {
-			if (parameter instanceof Variable && unaryOperator.isOperandNamed())
-				parameter = new ConstantImpl(((Variable) parameter).getName(),
-						parameter.getLocation());
-			unaryOperator.setOperand(parameter);
-			try {
-				// 常量优化算法，将常量先计算，如：
-				// ! true 将被优化为 false
-				if (parameter instanceof Constant)
-					return new ConstantImpl(unaryOperator.evaluate(null),
-							unaryOperator.getLocation());
-			} catch (Exception e) {
-				// 抛出任何异常都表示其不支持优化
-			}
-			return unaryOperator;
-		}
-
-		/**
-		 * 弹出(最后的)结果
-		 *
-		 * @return 结果
-		 */
-		Expression popResult() {
-			while (!operatorStack.isEmpty())
-				popOperator(); // 弹出栈中所有操作符
-			Expression result = (Expression) parameterStack.pop();
-			Assert.assertTrue(parameterStack.isEmpty(), "ExpressionReducer.operator.miss.parameter"); // 后验条件
-			return result;
-		}
-
 	}
 
 }
