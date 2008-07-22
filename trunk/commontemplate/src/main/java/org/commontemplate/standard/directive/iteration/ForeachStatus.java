51ec5ee1d82b2d40401b0f4bdcefcbd537a0e7d7
liangfei0201 <liangfei0201@8dadb90d-c23f-0410-a79a-e763b8a9fd85>
2008-07-22 06:47:56 +0000

    迭代状态增加递归级别${for.level}

--- trunk/commontemplate/src/main/java/org/commontemplate/standard/directive/iteration/ForeachStatus.java
+++ trunk/commontemplate/src/main/java/org/commontemplate/standard/directive/iteration/ForeachStatus.java
@@ -1,23 +1,41 @@
 package org.commontemplate.standard.directive.iteration;
 
+/**
+ * 迭代状态信息
+ *
+ * @author liangfei0201@163.com
+ *
+ */
 public class ForeachStatus {
 
-	private int index = 0;
+	private final int total;
 
-	private int total = 0;
+	private final int level;
 
 	public ForeachStatus(int total) {
 		this.total = total;
+		this.level = 0;
 	}
+
+	public ForeachStatus(int total, int level) {
+		this.total = total;
+		this.level = level;
+	}
+
+	private int index = 0;
 
 	public void increment() {
 		index ++;
 	}
-	
+
+	public int getLevel() {
+		return level;
+	}
+
 	public int getSize() {
 		return total;
 	}
-	
+
 	public int getIndex() {
 		return index;
 	}
@@ -33,11 +51,11 @@ public class ForeachStatus {
 	public boolean isLast() {
 		return index == total - 1;
 	}
-	
+
 	public boolean isMiddle() {
 		return index > 0 && index < total - 1;
 	}
-	
+
 	public boolean isEven() {
 		return (index + 1) % 2 == 0;
 	}
