038608a3b7aeebfecba0981b253b7ebd0c7b3264
liangfei0201 <liangfei0201@8dadb90d-c23f-0410-a79a-e763b8a9fd85>
2008-07-23 09:35:03 +0000

    加入检查$if和$for的表达式不能为空.

--- trunk/commontemplate/src/main/java/org/commontemplate/standard/directive/condition/ElseIfDirectiveHandler.java
+++ trunk/commontemplate/src/main/java/org/commontemplate/standard/directive/condition/ElseIfDirectiveHandler.java
@@ -2,9 +2,10 @@ package org.commontemplate.standard.directive.condition;
 
 import java.util.List;
 
+import org.commontemplate.config.MiddleBlockDirectiveHandler;
+import org.commontemplate.core.BlockDirective;
 import org.commontemplate.core.Context;
 import org.commontemplate.standard.directive.DirectiveUtils;
-import org.commontemplate.standard.directive.MiddleBlockDirectiveHandlerSupport;
 import org.commontemplate.util.Assert;
 import org.commontemplate.util.TypeUtils;
 
@@ -14,9 +15,17 @@ import org.commontemplate.util.TypeUtils;
  * @author liangfei0201@163.com
  *
  */
-public class ElseIfDirectiveHandler extends MiddleBlockDirectiveHandlerSupport {
+public class ElseIfDirectiveHandler extends MiddleBlockDirectiveHandler {
 
 	private static final long serialVersionUID = 1L;
+
+	public void doRender(Context context, BlockDirective directive) throws Exception {
+		Assert.assertNotNull(directive.getExpression(), "ElseIfDirectiveHandler.expression.is.null");
+		doRender(context, directive.getName(),
+				directive.getExpression() == null
+					? null : directive.getExpression().evaluate(context),
+				directive.getElements());
+	}
 
 	public void doRender(Context context, String directiveName, Object param, List innerElements) throws Exception {
 		Assert.assertNotNull(context.getSuperLocalContext().getStatus(IfDirectiveHandler.IF_STATUS), "ElseIfDirectiveHandler.location.error");
