fe9152890af8f02f2174c074a66fa4d7547007af
liangfei0201 <liangfei0201@8dadb90d-c23f-0410-a79a-e763b8a9fd85>
2008-07-25 03:11:36 +0000

    修复StringConvertUtils的BUG

--- commontemplate/src/test/java/org/commontemplate/util/StringConvertTester.java
+++ commontemplate/src/test/java/org/commontemplate/util/StringConvertTester.java
@@ -9,29 +9,71 @@ public class StringConvertTester extends TestCase {
 		super.assertEquals("\t", StringConvertUtils.convertLiteral("\\t"));
 		super.assertEquals("\f", StringConvertUtils.convertLiteral("\\f"));
 		super.assertEquals("\r", StringConvertUtils.convertLiteral("\\r"));
-		super.assertEquals("\\", StringConvertUtils.convertLiteral("\\\\"));
+		super.assertEquals("\b", StringConvertUtils.convertLiteral("\\b"));
 		super.assertEquals("\u1234", StringConvertUtils.convertLiteral("\\u1234"));
+		super.assertEquals("\\users", StringConvertUtils.convertLiteral("\\users"));
+		super.assertEquals("\\", StringConvertUtils.convertLiteral("\\\\"));
+		super.assertEquals("\"", StringConvertUtils.convertLiteral("\\\""));
+		super.assertEquals("\'", StringConvertUtils.convertLiteral("\\\'"));
 
 		super.assertEquals("aaa\n", StringConvertUtils.convertLiteral("aaa\\n"));
 		super.assertEquals("aaa\t", StringConvertUtils.convertLiteral("aaa\\t"));
 		super.assertEquals("aaa\f", StringConvertUtils.convertLiteral("aaa\\f"));
 		super.assertEquals("aaa\r", StringConvertUtils.convertLiteral("aaa\\r"));
-		super.assertEquals("aaa\\", StringConvertUtils.convertLiteral("aaa\\\\"));
+		super.assertEquals("aaa\b", StringConvertUtils.convertLiteral("aaa\\b"));
 		super.assertEquals("aaa\u1234", StringConvertUtils.convertLiteral("aaa\\u1234"));
+		super.assertEquals("aaa\\users", StringConvertUtils.convertLiteral("aaa\\users"));
+		super.assertEquals("aaa\\", StringConvertUtils.convertLiteral("aaa\\\\"));
+		super.assertEquals("aaa\"", StringConvertUtils.convertLiteral("aaa\\\""));
+		super.assertEquals("aaa\'", StringConvertUtils.convertLiteral("aaa\\\'"));
 
-		super.assertEquals("\nbbb", StringConvertUtils.convertLiteral("\\nbbb"));
-		super.assertEquals("\tbbb", StringConvertUtils.convertLiteral("\\tbbb"));
-		super.assertEquals("\fbbb", StringConvertUtils.convertLiteral("\\fbbb"));
-		super.assertEquals("\rbbb", StringConvertUtils.convertLiteral("\\rbbb"));
-		super.assertEquals("\\bbb", StringConvertUtils.convertLiteral("\\\\bbb"));
-		super.assertEquals("\u1234bbb", StringConvertUtils.convertLiteral("\\u1234bbb"));
+		super.assertEquals("\nccc", StringConvertUtils.convertLiteral("\\nccc"));
+		super.assertEquals("\tccc", StringConvertUtils.convertLiteral("\\tccc"));
+		super.assertEquals("\fccc", StringConvertUtils.convertLiteral("\\fccc"));
+		super.assertEquals("\rccc", StringConvertUtils.convertLiteral("\\rccc"));
+		super.assertEquals("\bccc", StringConvertUtils.convertLiteral("\\bccc"));
+		super.assertEquals("\u1234ccc", StringConvertUtils.convertLiteral("\\u1234ccc"));
+		super.assertEquals("\\usersccc", StringConvertUtils.convertLiteral("\\usersccc"));
+		super.assertEquals("\\ccc", StringConvertUtils.convertLiteral("\\\\ccc"));
+		super.assertEquals("\"ccc", StringConvertUtils.convertLiteral("\\\"ccc"));
+		super.assertEquals("\'ccc", StringConvertUtils.convertLiteral("\\\'ccc"));
 
-		super.assertEquals("aaa\nbbb", StringConvertUtils.convertLiteral("aaa\\nbbb"));
-		super.assertEquals("aaa\tbbb", StringConvertUtils.convertLiteral("aaa\\tbbb"));
-		super.assertEquals("aaa\fbbb", StringConvertUtils.convertLiteral("aaa\\fbbb"));
-		super.assertEquals("aaa\rbbb", StringConvertUtils.convertLiteral("aaa\\rbbb"));
-		super.assertEquals("aaa\\bbb", StringConvertUtils.convertLiteral("aaa\\\\bbb"));
-		super.assertEquals("aaa\u1234bbb", StringConvertUtils.convertLiteral("aaa\\u1234bbb"));
+		super.assertEquals("aaa\nccc", StringConvertUtils.convertLiteral("aaa\\nccc"));
+		super.assertEquals("aaa\tccc", StringConvertUtils.convertLiteral("aaa\\tccc"));
+		super.assertEquals("aaa\fccc", StringConvertUtils.convertLiteral("aaa\\fccc"));
+		super.assertEquals("aaa\rccc", StringConvertUtils.convertLiteral("aaa\\rccc"));
+		super.assertEquals("aaa\bccc", StringConvertUtils.convertLiteral("aaa\\bccc"));
+		super.assertEquals("aaa\u1234ccc", StringConvertUtils.convertLiteral("aaa\\u1234ccc"));
+		super.assertEquals("aaa\\usersccc", StringConvertUtils.convertLiteral("aaa\\usersccc"));
+		super.assertEquals("aaa\\ccc", StringConvertUtils.convertLiteral("aaa\\\\ccc"));
+		super.assertEquals("aaa\"ccc", StringConvertUtils.convertLiteral("aaa\\\"ccc"));
+		super.assertEquals("aaa\'ccc", StringConvertUtils.convertLiteral("aaa\\\'ccc"));
+
+	}
+
+	public void testRevertEscape() {
+		super.assertEquals("\\n", StringConvertUtils.revertLiteral(StringConvertUtils.convertLiteral("\\n")));
+		super.assertEquals("\\t", StringConvertUtils.revertLiteral(StringConvertUtils.convertLiteral("\\t")));
+		super.assertEquals("\\f", StringConvertUtils.revertLiteral(StringConvertUtils.convertLiteral("\\f")));
+		super.assertEquals("\\r", StringConvertUtils.revertLiteral(StringConvertUtils.convertLiteral("\\r")));
+		super.assertEquals("\\b", StringConvertUtils.revertLiteral(StringConvertUtils.convertLiteral("\\b")));
+		super.assertEquals("\\u1234", StringConvertUtils.revertLiteral(StringConvertUtils.convertLiteral("\\u1234")));
+		super.assertEquals("\\users", StringConvertUtils.revertLiteral(StringConvertUtils.convertLiteral("\\users")));
+		super.assertEquals("\\", StringConvertUtils.revertLiteral(StringConvertUtils.convertLiteral("\\\\")));
+		super.assertEquals("\"", StringConvertUtils.revertLiteral(StringConvertUtils.convertLiteral("\\\"")));
+		super.assertEquals("\'", StringConvertUtils.revertLiteral(StringConvertUtils.convertLiteral("\\\'")));
+
+		super.assertEquals("aaa\\nccc", StringConvertUtils.revertLiteral(StringConvertUtils.convertLiteral("aaa\\nccc")));
+		super.assertEquals("aaa\\tccc", StringConvertUtils.revertLiteral(StringConvertUtils.convertLiteral("aaa\\tccc")));
+		super.assertEquals("aaa\\fccc", StringConvertUtils.revertLiteral(StringConvertUtils.convertLiteral("aaa\\fccc")));
+		super.assertEquals("aaa\\rccc", StringConvertUtils.revertLiteral(StringConvertUtils.convertLiteral("aaa\\rccc")));
+		super.assertEquals("aaa\\bccc", StringConvertUtils.revertLiteral(StringConvertUtils.convertLiteral("aaa\\bccc")));
+		super.assertEquals("aaa\\u1234ccc", StringConvertUtils.revertLiteral(StringConvertUtils.convertLiteral("aaa\\u1234ccc")));
+		super.assertEquals("aaa\\usersccc", StringConvertUtils.revertLiteral(StringConvertUtils.convertLiteral("aaa\\usersccc")));
+		super.assertEquals("aaa\\ccc", StringConvertUtils.revertLiteral(StringConvertUtils.convertLiteral("aaa\\\\ccc")));
+		super.assertEquals("aaa\"ccc", StringConvertUtils.revertLiteral(StringConvertUtils.convertLiteral("aaa\\\"ccc")));
+		super.assertEquals("aaa\'ccc", StringConvertUtils.revertLiteral(StringConvertUtils.convertLiteral("aaa\\\'ccc")));
+
 	}
 
 	public void testConvertPath() {
