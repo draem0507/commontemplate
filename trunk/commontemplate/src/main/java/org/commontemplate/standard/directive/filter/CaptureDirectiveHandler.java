316b6e76e8103d300383fd2ac09ae5d3a2cd681a
liangfei0201 <liangfei0201@8dadb90d-c23f-0410-a79a-e763b8a9fd85>
2008-07-28 09:32:16 +0000

    增加$capture指令, 用于捕获输出到变量中.

---
+++ trunk/commontemplate/src/main/java/org/commontemplate/standard/directive/filter/CaptureDirectiveHandler.java
@@ -0,0 +1,35 @@
+package org.commontemplate.standard.directive.filter;
+
+import java.util.List;
+
+import org.commontemplate.core.Context;
+import org.commontemplate.standard.directive.BlockDirectiveHandlerSupport;
+import org.commontemplate.standard.directive.DirectiveUtils;
+import org.commontemplate.standard.filter.BufferedFilter;
+import org.commontemplate.util.Assert;
+
+/**
+ * 捕获内部块输出，保存到指定的变量中。如：
+ * $capture{"xxx"}
+ *     ...
+ * $end
+ *
+ * @author liangfei0201@163.com
+ *
+ */
+public class CaptureDirectiveHandler extends BlockDirectiveHandlerSupport {
+
+	private static final long serialVersionUID = 1L;
+
+	public void doRender(Context context, String directiveName, Object param, List innerElements) throws Exception {
+		Assert.assertTrue(param instanceof String);
+		String var = (String)param;
+		BufferedFilter bufferedFilter = new BufferedFilter();
+		context.setOutputFilter(bufferedFilter);
+		DirectiveUtils.renderAll(innerElements, context);
+		context.removeOutputFilter();
+		String value = bufferedFilter.getBuffered();
+		context.putVariable(var, value);
+	}
+
+}
