0dcb976c54ded6809d23bc1cdc7bcf133df635b7
liangfei0201 <liangfei0201@8dadb90d-c23f-0410-a79a-e763b8a9fd85>
2008-08-13 08:04:36 +0000

    修复MapEntry

--- trunk/commontemplate/src/main/java/org/commontemplate/util/MapEntry.java
+++ trunk/commontemplate/src/main/java/org/commontemplate/util/MapEntry.java
@@ -8,9 +8,9 @@ public class MapEntry implements Map.Entry, Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Object key;
-	
+
 	private Object value;
-	
+
 	public MapEntry(Object key, Object value) {
 		this.key = key;
 		this.value = value;
@@ -29,25 +29,40 @@ public class MapEntry implements Map.Entry, Serializable {
 		this.value = value;
 		return oldValue;
 	}
-	
+
 	public String toString() {
 		return key + ":" + value;
 	}
-	
-	public boolean equals(Object o) {
-		if (o == null) 
-			return false;
-		if (this == o) 
-			return true;
-		if (! (o instanceof MapEntry))
-			return false;
-		MapEntry e = (MapEntry)o;
-		return ((key == null && e.key == null) || (key != null && e.key != null && key.equals(e.key))) 
-				&& ((value == null && e.value == null) || (value != null && e.value != null && value.equals(e.key)));
-	}
-	
+
+	// 以下hashCode和equals由eclipse自动生成
+
 	public int hashCode() {
-		return 37 * (key.hashCode() + (value!=null?value.hashCode():0));
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((key == null) ? 0 : key.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
 	}
-	
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final MapEntry other = (MapEntry) obj;
+		if (key == null) {
+			if (other.key != null)
+				return false;
+		} else if (!key.equals(other.key))
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
+
 }
