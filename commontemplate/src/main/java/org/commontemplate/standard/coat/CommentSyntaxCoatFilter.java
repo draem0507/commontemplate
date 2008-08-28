08d574b647c33fec3494afc7b62e6d52d11c88b5
liangfei0201 <liangfei0201@8dadb90d-c23f-0410-a79a-e763b8a9fd85>
2008-08-28 02:34:26 +0000

    注释语法外套加入clearSpaceline

--- commontemplate/src/main/java/org/commontemplate/standard/coat/CommentSyntaxCoatFilter.java
+++ commontemplate/src/main/java/org/commontemplate/standard/coat/CommentSyntaxCoatFilter.java
@@ -16,6 +16,7 @@ public class CommentSyntaxCoatFilter implements TextFilter, java.io.Serializable
 
 	private static final long serialVersionUID = 1L;
 
+	// 注释起始符
 	private String begin = "";
 
 	public String getBegin() {
@@ -28,6 +29,7 @@ public class CommentSyntaxCoatFilter implements TextFilter, java.io.Serializable
 		this.begin = begin;
 	}
 
+	// 注释结束符
 	private String end = "";
 
 	public String getEnd() {
@@ -40,11 +42,28 @@ public class CommentSyntaxCoatFilter implements TextFilter, java.io.Serializable
 		this.end = end;
 	}
 
+	// 当指令所在行没有其它内容时，清除该空白行
+	private boolean clearSpaceline;
+
+	public boolean isClearSpaceline() {
+		return clearSpaceline;
+	}
+
+	public void setClearSpaceline(boolean clearSpaceline) {
+		this.clearSpaceline = clearSpaceline;
+	}
+
 	public String filter(String text, boolean isFirst, boolean isLast) {
-		if (text.startsWith(end))
+		if (text.startsWith(end)) {
 			text = trimEnd(text);
-		if (text.endsWith(begin))
+			if (clearSpaceline)
+				text = clearEnd(text);
+		}
+		if (text.endsWith(begin)) {
 			text = trimBegin(text);
+			if (clearSpaceline)
+				text = clearBegin(text);
+		}
 		return text;
 	}
 
@@ -54,6 +73,36 @@ public class CommentSyntaxCoatFilter implements TextFilter, java.io.Serializable
 
 	protected String trimBegin(String text) {
 		return text.substring(0, text.length() - begin.length());
+	}
+
+	protected String clearEnd(String text) {
+		for (int i = 0, n = text.length(); i < n; i ++) {
+			char ch = text.charAt(i);
+			if (ch == '\n') { // 如果遇到换行符，表示指令所在行没有其它内容，将空白去除
+				text = text.substring(i + 1);
+				break;
+			} else if (! isSpace(ch)) { // 如果遇到非空白符，表示指令所在行有其它内容，不作处理
+				break;
+			}
+		}
+		return text;
+	}
+
+	protected String clearBegin(String text) {
+		for (int i = text.length() - 1; i >= 0; i --) {
+			char ch = text.charAt(i);
+			if (ch == '\n') { // 如果遇到换行符，表示指令所在行没有其它内容，将空白去除(保留换行符)
+				text = text.substring(0, i + 1);
+				break;
+			} else if (! isSpace(ch)) { // 如果遇到非空白符，表示指令所在行有其它内容，不作处理
+				break;
+			}
+		}
+		return text;
+	}
+
+	private boolean isSpace(char ch) {
+		return (ch == ' ' || ch == '\t' || ch == '\r' || ch == '\f' || ch == '\b');
 	}
 
 }
