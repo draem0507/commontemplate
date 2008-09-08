4d3a869f582eb4413803763aa55a5744f049ea7d
liangfei0201 <liangfei0201@8dadb90d-c23f-0410-a79a-e763b8a9fd85>
2008-09-08 02:06:28 +0000

    $load指令加入指定编码支持. 如: $load{xml: "xxx.ctl", "utf-8"}

--- commontemplate/src/main/java/org/commontemplate/standard/directive/data/LoadDataDirectiveHandler.java
+++ commontemplate/src/main/java/org/commontemplate/standard/directive/data/LoadDataDirectiveHandler.java
@@ -1,5 +1,6 @@
 package org.commontemplate.standard.directive.data;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -30,18 +31,36 @@ public class LoadDataDirectiveHandler extends DirectiveHandlerSupport {
 	public void doRender(Context context, String directiveName, Object param) throws Exception {
 		if (param instanceof String) {
 			String path = (String)param;
-			int i = path.lastIndexOf('.');
-			Assert.assertTrue(i >= 0 && i < path.length() - 1, "必需有扩展名!"); // TODO 未国际化
-			loadData(context, path.substring(i + 1), path);
+			loadData(context, getSuffix(path), path, null);
 		} else if (param instanceof Entry) {
 			Entry entry = (Entry)param;
-			loadData(context, String.valueOf(entry.getKey()), String.valueOf(entry.getValue()));
+			loadData(context, String.valueOf(entry.getKey()), String.valueOf(entry.getValue()), null);
+		} else if (param instanceof List) {
+			List list = (List)param;
+			Assert.assertTrue(list.size() == 2, "$load指令参数错误!");
+			Assert.assertTrue(list.get(1) instanceof String, "$load指令参数错误!");
+			String encoding = (String)list.get(1);
+			if (list.get(0) instanceof String) {
+				String path = (String)list.get(0);
+				loadData(context, getSuffix(path), path, encoding);
+			} else if (list.get(0) instanceof Entry) {
+				Entry entry = (Entry)list.get(0);
+				loadData(context, String.valueOf(entry.getKey()), String.valueOf(entry.getValue()), encoding);
+			} else {
+				Assert.fail("$load指令参数错误!"); // TODO 未国际化
+			}
 		} else {
 			Assert.fail("$load指令参数错误!"); // TODO 未国际化
 		}
 	}
 
-	private void loadData(Context context, String type, String path) throws Exception {
+	private String getSuffix(String path) {
+		int i = path.lastIndexOf('.');
+		Assert.assertTrue(i >= 0 && i < path.length() - 1, "必需有扩展名!"); // TODO 未国际化
+		return path.substring(i + 1);
+	}
+
+	private void loadData(Context context, String type, String path, String encoding) throws Exception {
 		if (dataProviders == null)
 			throw new RuntimeException("没有注册dataProviders");
 		DataProvider dataProvider = (DataProvider)dataProviders.get((String)type);
@@ -50,7 +69,7 @@ public class LoadDataDirectiveHandler extends DirectiveHandlerSupport {
 					+ "数据类型. 配置中只支持数据类型: " + dataProviders.keySet()
 					+ ", 可以通过实现:"+DataProvider.class.getName()
 					+ "接口扩展数据供给类型. 然后在commontemplate.properties中注册, 如: dataProvider{xxx}=com.xxx.XXXDataProvider()"); // TODO 未国际化
-		Resource resource = context.loadResource(path);
+		Resource resource = context.loadResource(path, encoding);
 		Map data = dataProvider.getData(IOUtils.readToString(resource.getReader()));
 		context.getRootLocalContext().putAllVariables(data);
 	}
