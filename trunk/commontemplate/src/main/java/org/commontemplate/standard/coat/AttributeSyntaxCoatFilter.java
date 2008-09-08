b909c0d85a8c67e160ae047bf1863bedcaf834a9
liangfei0201 <liangfei0201@8dadb90d-c23f-0410-a79a-e763b8a9fd85>
2008-09-08 08:46:25 +0000

    增加TagSyntaxCoatFilter,待实现

--- trunk/commontemplate/src/main/java/org/commontemplate/standard/coat/AttributeSyntaxCoatFilter.java
+++ trunk/commontemplate/src/main/java/org/commontemplate/standard/coat/AttributeSyntaxCoatFilter.java
@@ -2,6 +2,22 @@ package org.commontemplate.standard.coat;
 
 import org.commontemplate.standard.coat.attribute.jericho.JerichoAttributeCoatFilter;
 
+/**
+ * HTML属性语法外套实现.<br>
+ * 将HTML标签的特殊属性转换为指令.<br>
+ * 如:<br>
+ * <pre>
+ * &lt;table ct:if="users != null && users.size &gt; 0" border="1"&gt;
+ *     &lt;tr ct:for="user : users"&gt;
+ *         &lt;td&gt;&lt;span ct:out="for.index + 1"&gt;1&lt;/span&gt;&lt;/td&gt;
+ *         &lt;td&gt;&lt;span ct:out="user.name"&gt;james&lt;/span&gt;&lt;/td&gt;
+ *         &lt;td&gt;&lt;span ct:out="user.coins"&gt;2.00&lt;/span&gt;&lt;/td&gt;
+ *     &lt;/tr&gt;
+ * &lt;/table&gt;
+ * </pre>
+ *
+ * @author GuiLeen
+ */
 public class AttributeSyntaxCoatFilter extends JerichoAttributeCoatFilter {
 
 	private static final long serialVersionUID = 1L;
---
+++ trunk/commontemplate/src/main/java/org/commontemplate/standard/filter/StringResourceFilter.java
@@ -0,0 +1,25 @@
+package org.commontemplate.standard.filter;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Serializable;
+import java.io.StringReader;
+
+import org.commontemplate.config.ResourceFilter;
+import org.commontemplate.util.IOUtils;
+
+/**
+ * Reader转为String过滤
+ *
+ * @author liangfei0201@163.com
+ *
+ */
+public abstract class StringResourceFilter implements ResourceFilter, Serializable {
+
+	public Reader filter(Reader reader) throws IOException {
+		return new StringReader(filter(IOUtils.readToString(reader)));
+	}
+
+	public abstract String filter(String text);
+
+}
