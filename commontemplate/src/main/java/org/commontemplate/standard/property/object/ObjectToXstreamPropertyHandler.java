bd07275b5275d31ad0f010d6f69a6a79c69f5e68
liangfei0201 <liangfei0201@8dadb90d-c23f-0410-a79a-e763b8a9fd85>
2008-11-12 02:54:13 +0000

    修复toXstream属性

--- commontemplate/src/main/java/org/commontemplate/standard/property/object/ObjectToXstreamPropertyHandler.java
+++ commontemplate/src/main/java/org/commontemplate/standard/property/object/ObjectToXstreamPropertyHandler.java
@@ -3,13 +3,14 @@ package org.commontemplate.standard.property.object;
 import org.commontemplate.standard.property.PropertyHandlerSupport;
 
 import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 
 public class ObjectToXstreamPropertyHandler extends PropertyHandlerSupport {
 
 	private static final long serialVersionUID = 1L;
 
 	public Object doProperty(Object bean) throws Exception {
-		return new XStream().toXML(bean);
+		return new XStream(new DomDriver()).toXML(bean);
 	}
 
 }
